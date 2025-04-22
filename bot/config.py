50e5794c65b7ad52f0eb837eb30911d6acb5b8d7
VladTichonenko <vlad6867911@gmail.com>
2025-04-22 14:04:18 +0300

    c

--- bot/config.py
+++ bot/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = "7607756648:AAF2293fe7XxDbOsKB_gPiIuPcDcloe8yZw"
-ADMINS = [1127948594]  # Ваш Telegram ID
+ADMINS = [765843635]  # Ваш Telegram ID
 BOT_USERNAME = "QrPay_oficial_bot"  # Без @
